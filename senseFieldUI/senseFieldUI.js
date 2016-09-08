6a73f6e1f0fb713b82b1170cff022db3f93b05ca
Alex Byrd <balexbyrd@users.noreply.github.com>
2016-09-08 15:05:07 -0400

    Updated to current plus merge

--- senseFieldUI/senseFieldUI.js
+++ senseFieldUI/senseFieldUI.js
@@ -264,6 +264,263 @@ define([
                                             value: "btn-md",
                                             label: "Medium"
 										}, {
+<<<<<<< HEAD
+											value: "checkbox-circle",
+											label: "Circle"
+										}]
+									,
+									show : function(data) {
+										if(data.vars.StyleOverride === 'Checkbox' ){
+											return data.vars.StyleOverride;
+										}
+									}
+								},								
+								Defaulthighlight : {
+									ref : "vars.Defaulthighlight",
+									type : "boolean",
+									label : "Always One Selected value",
+									defaultValue : false,
+									show : function(data) {
+										if(!data.vars.dropMultiSelect){
+											return data.vars.StyleOverride;
+										}
+									}									
+								},								
+								Defaulthighlightvalue:{
+									ref: "vars.Defaulthighlightvalue",
+									translation: "Default Selected Value *",
+									type: "string",
+									defaultValue: " ",
+									show : function(data) {
+										return data.vars.Defaulthighlight;
+									}
+								},
+								hiddenVar : {
+									ref : "vars.hiddenField",
+									type : "boolean",
+									label : "Hidden Field? ( HidePrefix )",
+									defaultValue : false,
+									show : function(data) {
+										return data.vars.Defaulthighlight;
+									}										
+								},
+								disableFullScreen: {
+									ref: "disableFullScreen",
+									type: "boolean",
+									component: "checkbox",
+									label: "Disable Full Screen Option (press F5 after change)",
+									defaultValue: false
+								}								
+							}							
+						}
+					}
+				}
+			}
+		},	
+/* 		snapshot: {
+			canTakeSnapshot: true
+		}, */
+		paint: function ( $element , layout) {
+			
+			var html = '',
+				app = qlik.currApp(),
+				btnColor = ["default","info","primary","success","warning","danger"][layout.vars.selectedColor], 
+				icons = layout.vars.iconButton,
+				vars = {
+					v: '1.0.0',
+					id: layout.qInfo.qId,
+					field: [],	
+					fieldSize: [],
+					iconOnly: layout.vars.iconOnly,
+					icons: icons.split(","),
+					btnColor: btnColor,
+					btnSize: layout.vars.btnSize,
+					btnSpan: layout.vars.btnSpan,
+					oneSelected: layout.vars.Defaulthighlight,
+					chbxStyle: layout.vars.chbxStyle,
+					dimSelected: layout.vars.Defaulthighlightvalue,
+					hiddenField: layout.vars.hiddenField,
+					dropMultiSelect: layout.vars.dropMultiSelect,
+					ListType: layout.vars.ListType,
+					styletype: layout.vars.StyleOverride,
+					disableFullScreen: layout.disableFullScreen,
+					this: this
+				},
+				$bootstrapStyle = $( document.createElement( 'div' ) ).attr( 'id', vars.id  ).addClass( 'bootstrap_inside' ),
+				elemNo;
+			
+			var n = vars.id, styles = vars.styletype; styles = styles.toLowerCase(), tempId[n] = [];
+			
+			vars.field.push(layout.qListObject.qDimensionInfo.qFallbackTitle);
+			vars.fieldSize.push(layout.qListObject.qDimensionInfo.qCardinal);
+			
+			app.getList("CurrentSelections", function(reply) {	 
+				if(reply.qSelectionObject.qSelections.length === 0){
+					if(!vars.oneSelected){
+						tempId[n] = [];
+					}else if(!vars.hiddenField){
+						selectFirst(elemNo);
+					};
+				};
+			});			
+			
+			// Radio
+			if(styles==='radio'){				
+				this.backendApi.eachDataRow(function(rownum, row) {		
+					if(row[0].qState==='S'){var checkedstatus ='checked'; }else{var checkedstatus ='';}
+					if(row[0].qState==='X'){var dis = 'disabled';}else{var dis = '';};
+					if(vars.oneSelected && row[0].qText === vars.dimSelected){elemNo = row[0].qElemNumber; };
+					
+					html += '<div class="radio radio-'+btnColor+' radio-'+vars.ListType+' '+dis+'"><input type="radio" name="'+n+'_'+styles+'" id='+n+'_'+row[0].qElemNumber+' '+checkedstatus+' '+dis+'><label class="inline control-label" >'+row[0].qText+'</label></div>';
+				});				
+			// Checkbox	
+			}else if(styles==='checkbox'){
+				this.backendApi.eachDataRow(function(rownum, row) {	
+					if(row[0].qState==='S'){var checkedstatus ='checked'; }else{var checkedstatus ='';}
+					if(row[0].qState==='X'){var dis = 'disabled';}else{var dis = '';};
+					if(vars.oneSelected && row[0].qText === vars.dimSelected){elemNo = row[0].qElemNumber; };
+					
+					html += '<div class="checkbox '+vars.chbxStyle+' checkbox-'+btnColor+' checkbox-'+vars.ListType+' '+dis+'"><input class="styled" type="checkbox" id='+n+'_'+row[0].qElemNumber+' '+checkedstatus+' '+dis+'><label >'+row[0].qText+'</label></div>';
+				});			
+			// Dropdown	
+			}else if(styles==='dropdown'){
+				if(vars.dropMultiSelect){vars.dropDownStyle = 'multiple'}else{vars.dropDownStyle = ''};
+				html += '<row>'
+				html += '	<div class="col-md-12">';
+				html += '		<div class="form-group">';
+				html += '			<select '+vars.dropDownStyle+' class="form-control">';			
+					this.backendApi.eachDataRow(function(rownum, row) {
+						if(row[0].qState==='S'){var active ='lightgreen'; var actv = 'selected'}else{var active ='', actv = '';};
+						if(row[0].qState==='X'){var dis = 'disabled';}else{var dis = '';};
+						if(vars.oneSelected && row[0].qText === vars.dimSelected){elemNo = row[0].qElemNumber; };						
+						html += '<option  id='+n+'_'+row[0].qElemNumber+' style="background-color: '+active+'" '+actv+' '+dis+'>'+row[0].qText+'</option>';
+					});
+				html += '			</select>';				
+				html += '		</div>';
+				html += '	</div>';
+				html += '</row>';
+			
+			// Button	
+			}else {
+				var rw = 0;
+				if(vars.btnSpan){
+					var spn = 'style="width: 100%; white-space: normal;"',
+						spnBtn = 'style="width: '+100/vars.fieldSize+'%; white-space: normal;"'
+				}else{
+					var spn = '', spnBtn = '';
+				}
+				html += '<div class="btn-group" '+spn+'>'
+					this.backendApi.eachDataRow(function(rownum, row) {	
+						if(row[0].qState==='S'){var active ='btn-'+btnColor; tempId[n].push(row[0].qElemNumber);}else{var active ='btn-default';};
+						if(row[0].qState==='X'){var dis = 'disabled';}else{var dis = '';};
+						if(vars.ListType===' '){var ListType ='btn-block'; }else{var ListType ='';}; 
+						if(vars.oneSelected && row[0].qText === vars.dimSelected){elemNo = row[0].qElemNumber; };
+						if(vars.iconOnly){var icon = '<i id="sel_'+row[0].qElemNumber+'" class="fa fa-'+vars.icons[rownum]+'"></i>'}else {var icon = row[0].qText};
+						html += '<button '+spnBtn+' id='+vars.id+'_'+row[0].qElemNumber+' class="btn '+active+' '+vars.btnSize+' '+ListType+' text-center" type="button" title="'+row[0].qText+'" '+dis+'>'+icon+'</button>';
+						rw += 1;
+					});				
+				html += '</div>';
+			};
+			$bootstrapStyle.html( html );
+			$element.html( $bootstrapStyle );										
+			
+			if(vars.oneSelected && control === 0){
+				tempId[n] = [];
+				selectFirst(elemNo);
+				control++
+			};		
+			
+			function selectFirst(dim){
+				vars.this.backendApi.clearSelections();
+				tempId[n].push(dim);
+				vars.this.backendApi.selectValues(0, [dim], false);
+			};
+			
+			// Radio or Checkbox
+			$('#'+n+' .'+styles).on('click',function() {
+				
+				var lastChar,
+					id = this.firstChild.id,
+					lastChar = Number(id.split('_')[1]),
+					i = tempId[n].indexOf(lastChar),
+					s = $('#'+vars.id+' #'+id).prop('checked');
+				
+				if(styles === 'radio' || vars.oneSelected){		
+					tempId[n] = [lastChar];
+					vars.bool = false;
+				}else {
+					if(i>-1){					
+						tempId[n].splice(i, 1);						
+					}else {
+						if(!isNaN(lastChar)){tempId[n].push(lastChar)};
+					};
+					vars.bool = true;
+				}				
+				tempId[n] = _.uniq(tempId[n]);			
+				//console.info('Style: '+styles+ '\n' +'In array: '+i+'\n'+'Array: ' +tempId[n]+ '\n'+'Elmnt Number: ' +lastChar+ '\n'+'ID: ' +id+ '\n' +'Property Checked: '+s+ '\n'+'One Selected: ' +vars.oneSelected);
+				vars.this.backendApi.selectValues(0, tempId[n], vars.bool);
+			});
+			
+			// Buttons
+			$('#'+vars.id+' button').on('click',function(d) {				
+				var id = d.target.id,
+					lastChar = Number(id.split('_')[1]),
+					i = tempId[n].indexOf(lastChar);				
+								
+				if(i>-1){					
+					tempId[n].splice(i, 1);
+				}else{
+					if(!isNaN(lastChar)){tempId[n].push(lastChar)};
+				}
+				if(vars.oneSelected){tempId[n] = [lastChar];};				
+				tempId[n] = _.uniq(tempId[n]);
+				//console.info(styles,i, tempId[n],lastChar, id, vars.oneSelected);
+				vars.this.backendApi.selectValues(0, tempId[n], false);
+			});
+			
+			//Dropdown
+			$('#'+vars.id+' .form-control').change(function(d) {				
+				var id = $(this).children(":selected").attr("id"),
+					lastChar = Number(id.split('_')[1]),
+					i = tempId[n].indexOf(lastChar);
+								
+				if(vars.dropMultiSelect){										
+					var dm = $('.form-control').val();
+					app.field(vars.field[0]).selectValues(dm, false);
+				}else{
+					if(i>-1){					
+						tempId[n].splice(i, 1);
+					}else{
+						if(!isNaN(lastChar)){tempId[n].push(lastChar)};
+					}										
+					if(vars.oneSelected){tempId[n] = [lastChar]; vars.bool = false}else{vars.bool = true};	
+					tempId[n] = _.uniq(tempId[n]);				
+					
+					//console.info(id,lastChar,i, tempId[n],vars.oneSelected);
+					vars.this.backendApi.selectValues(0, tempId[n], vars.bool);				
+				};
+			});
+			
+			// Clear Selections
+			$('#clearselections').on('click',function() {				
+				tempId[n] = [];				
+				if(vars.oneSelected){
+					app.field(vars.field[0]).selectMatch(vars.dimSelected, true);
+				};
+			});
+
+			// switch Full Screen option
+			if (vars.disableFullScreen) {
+				// prevent multiple style elements
+				if ($('#senseFieldUI-fullScreen').length == 0) {
+					$("<style type='text/css' id='senseFieldUI-fullScreen'>.qv-object-senseFieldUI ~ .qv-object-nav .icon-zoom-in {display: none;}</style>").appendTo("head");
+				}
+			}			
+		
+		}
+	};
+} );
+=======
                                             value: "btn-lg",
                                             label: "Large"
 										}],
@@ -620,6 +877,7 @@ define([
                         app.field(vars.field[0]).selectMatch(vars.dimSelected, true);
                     };
                 });
+>>>>>>> origin/master
 
                 // switch Full Screen option
                 if (vars.disableFullScreen) {
